fd8fe82edd71a518c0d6d61cdf9d3a00dabca30d
fatima bouchtaoui <fatima.bouchtaoui@utb.ecutbildning.se>
2022-11-29 14:44:52 +0100

    Implemented CrossOrigin

--- src/main/java/me/code/uppgift3projekt/controller/AuthController.java
+++ src/main/java/me/code/uppgift3projekt/controller/AuthController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 @AllArgsConstructor
 public class AuthController {
